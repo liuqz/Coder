98424be308c163ebfb5fdb0c106b47b3a533acfc
liuqz <xtliuqz@163.com>
2017-06-23 09:03:57 +0800

    init

---
+++ src/main/java/com/wftest/decode/Decode.java
@@ -0,0 +1,7 @@
+package com.wftest.decode;
+
+/**
+ * Created by Liuqz on 2017-6-22.
+ */
+public class Decode {
+}
---
+++ src/main/java/com/wftest/encode/Encode.java
@@ -0,0 +1,7 @@
+package com.wftest.encode;
+
+/**
+ * Created by Liuqz on 2017-6-22.
+ */
+public class Encode {
+}
