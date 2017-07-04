5a5b808c65d54d05274cf14ab5f32879edf6eca9
liuqz <xtliuqz@163.com>
2017-07-04 15:13:01 +0800

    log4j2配置完成

---
+++ src/main/java/com/wfcode/arithmetic/AESCoder.java
@@ -0,0 +1,42 @@
+package com.wfcode.arithmetic;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
+/**
+ * AES安全编码组件
+ *
+ * <pre>
+ * 支持 DES、DESede(TripleDES,就是3DES)、AES、Blowfish、RC2、RC4(ARCFOUR)
+ * DES          		key size must be equal to 56
+ * DESede(TripleDES) 	key size must be equal to 112 or 168
+ * AES          		key size must be equal to 128, 192 or 256,but 192 and 256 bits may not be available
+ * Blowfish     		key size must be multiple of 8, and can only range from 32 to 448 (inclusive)
+ * RC2          		key size must be between 40 and 1024 bits
+ * RC4(ARCFOUR) 		key size must be between 40 and 1024 bits
+ * 具体内容 需要关注 JDK Document http://.../docs/technotes/guides/security/SunProviders.html
+ * </pre>
+ * Created by Liuqz on 2017-7-4.
+ */
+public class AESCoder {
+    /**
+     * ALGORITHM 算法 <br>
+     * 可替换为以下任意一种算法，同时key值的size相应改变。
+     *
+     * <pre>
+     * DES          		key size must be equal to 56
+     * DESede(TripleDES) 	key size must be equal to 112 or 168
+     * AES          		key size must be equal to 128, 192 or 256,but 192 and 256 bits may not be available
+     * Blowfish     		key size must be multiple of 8, and can only range from 32 to 448 (inclusive)
+     * RC2          		key size must be between 40 and 1024 bits
+     * RC4(ARCFOUR) 		key size must be between 40 and 1024 bits
+     * </pre>
+     */
+    public static final String ALGORITHM = "DES";
+
+    private static Key toKey(byte[] key) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(key, ALGORITHM);
+        return secretKey;
+    }
+}
